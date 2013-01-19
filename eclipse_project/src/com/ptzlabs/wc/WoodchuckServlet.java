97573dfb8b32f8c976034f45688be07bf5da9734
Peter Zhang <p@peetahzee.com>
2013-01-18 23:15:13 -0500

    initial commit, with objectify-appengine

---
+++ eclipse_project/src/com/ptzlabs/wc/WoodchuckServlet.java
@@ -0,0 +1,11 @@
+package com.ptzlabs.wc;
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class WoodchuckServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
