e638936429eff9098305c7733135408501707522
jjchen <junjunc@princeton.edu>
2013-01-19 14:44:49 -0500

    fix infinite loop?

--- eclipse_project/src/com/ptzlabs/wc/servlet/ReadingServlet.java
+++ eclipse_project/src/com/ptzlabs/wc/servlet/ReadingServlet.java
@@ -61,14 +61,14 @@ public class ReadingServlet extends HttpServlet {
 				String text = stripper.getText(document);
 
 				int fromIndex = 0;
-				String[] line_arr = text.split("\\. ");
+				int endSentence = text.indexOf(". ", fromIndex);
 				int sentence = 0;
-				int i = 0;
+				int fromIndex = 0;
 				String data = "";
 				
 				int chunkCounter = 1;
-				while (i < line_arr.length) {
-					data += line_arr[i];
+				while (endSentence != -1) {
+					data += text.subString(fromIndex, endSentence);
 					sentence++;
 					if (sentence == 2) {
 						Chunk chunk = new Chunk(chunkCounter, readingKey, data);
@@ -77,7 +77,8 @@ public class ReadingServlet extends HttpServlet {
 						sentence = 0;
 						data = "";
 					}
-					i++;
+					fromIndex = endSentence + 2;
+					endSentence = text.indexOf(". ", fromIndex);
 				}
 				if (sentence != 0) {
 					Chunk chunk = new Chunk(chunkCounter, readingKey, data);
@@ -116,7 +117,7 @@ public class ReadingServlet extends HttpServlet {
 					int i = 0;
 					int endSentence = line.indexOf(". ", fromIndex);
 					while (endSentence != -1) {
-						data += line.subString(fromIndex, endSentence);
+						data += line.substring(fromIndex, endSentence);
 						sentence++;
 						if (sentence == 2) {
 							Chunk chunk = new Chunk(chunkCounter, readingKey, data);
@@ -126,9 +127,10 @@ public class ReadingServlet extends HttpServlet {
 							chunkCounter++;
 						}
 						fromIndex = endSentence + 2;
+						endSentence = line.indexOf(". ", fromIndex);
 					}
 					
-					data += line.subString(fromIndex, sentence.length);
+					data += line.substring(fromIndex, sentence.length);
 					line = reader.readLine();
 				}
 
