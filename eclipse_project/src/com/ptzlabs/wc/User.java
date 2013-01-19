09a9baef5ef0567440ab39931eb8fdeca8629784
Peter Zhang <p@peetahzee.com>
2013-01-19 02:05:44 -0500

    add gson. parse json from facebook after checking access token

--- eclipse_project/src/com/ptzlabs/wc/User.java
+++ eclipse_project/src/com/ptzlabs/wc/User.java
@@ -1,12 +1,22 @@
 package com.ptzlabs.wc;
 
+import com.googlecode.objectify.annotation.Entity;
 import com.googlecode.objectify.annotation.Id;
 import com.googlecode.objectify.annotation.Index;
 
+@Entity
 public class User {
 	@Id Long id;
-	@Index Long fbid;
+	@Index long fbid;
 	String name;
 	String email;
 	int phone;
+	
+	private User() { }
+	
+	public User(long fbid, String name, String email) {
+		this.fbid = fbid;
+		this.name = name;
+		this.email = email;
+	}
 }
--- eclipse_project/src/com/ptzlabs/wc/servlet/LoginServlet.java
+++ eclipse_project/src/com/ptzlabs/wc/servlet/LoginServlet.java
@@ -1,15 +1,19 @@
 package com.ptzlabs.wc.servlet;
 
+import static com.googlecode.objectify.ObjectifyService.ofy;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLEncoder;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+import com.ptzlabs.wc.User;
 
 @SuppressWarnings("serial")
 public class LoginServlet extends HttpServlet {
@@ -21,20 +25,24 @@ public class LoginServlet extends HttpServlet {
 			return;
 		}
 		
-		resp.setContentType("text/plain");
-		
-		resp.getWriter().println("access_token: " + accessToken);
-		resp.getWriter().println("userid: " + userId);
-		
 		try {
-            URL url = new URL("http://graph.facebook.com/" + userId + 
+            URL url = new URL("https://graph.facebook.com/" + userId + 
             		"?fields=name,email&access_token=" + accessToken);
             BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
-            String line;
 
-            while ((line = reader.readLine()) != null) {
-                resp.getWriter().println(line);
+            // there is only one json line to read
+            String line = reader.readLine();
+            Gson gson = new Gson();
+            UserData data = gson.fromJson(line, UserData.class);
+            
+            if(data.error != null) {
+            	User user = new User(Long.parseLong(data.id), data.name, data.email);
+            	ofy().save().entity(user).now();
+            } else {
+            	resp.setContentType("text/plain");
+            	resp.getWriter().println("FB Error " + data.error.code + ": " + data.error.message);
             }
+            
             reader.close();
 
         } catch (MalformedURLException e) {
@@ -49,5 +57,23 @@ public class LoginServlet extends HttpServlet {
 			"&redirect_uri=" + URLEncoder.encode("http://wc.ptzlabs.com/login", "ISO-8859-1") +
 		    "&state=SOME_ARBITRARY_BUT_UNIQUE_STRING");
 		*/
+	}
+	
+	/**
+	 * Use only for gson conversion
+	 */
+	private class UserData {
+		String name;
+		String id; // should be parsed to a long
+		String email;
+		GraphError error;
+		UserData() { };
+		
+		private class GraphError {
+			String message;
+			String type;
+			int code;
+			GraphError() { };
+		}
 	}
 }
