a153eb5b008a5d0d3b6273f29a191c70677040f6
Peter Zhang <p@peetahzee.com>
2013-01-19 04:52:20 -0500

    read from net and store in blobstore

--- eclipse_project/src/com/ptzlabs/wc/servlet/ReadingServlet.java
+++ eclipse_project/src/com/ptzlabs/wc/servlet/ReadingServlet.java
@@ -2,7 +2,11 @@ package com.ptzlabs.wc.servlet;
 
 import static com.googlecode.objectify.ObjectifyService.ofy;
 
+import java.io.BufferedInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.ByteBuffer;
 import java.util.Date;
 
 import javax.servlet.http.HttpServlet;
@@ -10,33 +14,84 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.google.appengine.api.blobstore.BlobKey;
+import com.google.appengine.api.files.AppEngineFile;
+import com.google.appengine.api.files.FileService;
+import com.google.appengine.api.files.FileServiceFactory;
+import com.google.appengine.api.files.FileWriteChannel;
 import com.ptzlabs.wc.Reading;
 
 public class ReadingServlet extends HttpServlet {
-	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		
-		if(req.getParameter("mode").equals("new") && req.getParameter("name") != null &&
-				req.getParameter("location") != null && req.getParameter("fbid") != null) {
-			
+	public void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+
+		if (req.getParameter("mode").equals("new")
+				&& req.getParameter("name") != null
+				&& req.getParameter("location") != null
+				&& req.getParameter("fbid") != null) {
+
 			Reading reading;
-			if(req.getParameter("dueDate") != null) {
-				reading = new Reading(req.getParameter("name"),
-						new Date(Long.parseLong(req.getParameter("dueDate"))),
-						Long.parseLong(req.getParameter("fbid"))); 
+			if (req.getParameter("dueDate") != null) {
+				reading = new Reading(req.getParameter("name"), new Date(
+						Long.parseLong(req.getParameter("dueDate"))),
+						Long.parseLong(req.getParameter("fbid")));
 			} else {
 				reading = new Reading(req.getParameter("name"),
 						Long.parseLong(req.getParameter("fbid")));
 			}
-			
-			
+
 			ofy().save().entity(reading).now();
-			
+
 			resp.setContentType("text/plain");
 			resp.getWriter().println("OK");
 		}
 	}
-	
+
 	private static BlobKey readFileAndStore(String location) {
+		try {
+			// Get a file service
+			FileService fileService = FileServiceFactory.getFileService();
+
+			// Create a new Blob file with mime-type "text/plain"
+			AppEngineFile file = fileService.createNewBlobFile("text/plain");
+
+			// Open a channel to write to it
+			boolean lock = false;
+			FileWriteChannel writeChannel = fileService.openWriteChannel(file, lock);
+
+			InputStream in = new BufferedInputStream(new URL(location).openStream());
+
+			byte data[] = new byte[1024];
+			int count;
+			while ((count = in.read(data, 0, 1024)) != -1) {
+				writeChannel.write(ByteBuffer.wrap(data));
+			}
+			if (in != null) in.close();
+			writeChannel.closeFinally();
+			
+			return fileService.getBlobKey(file);
+		} catch (IllegalStateException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		return null;
+
+		/*
+		// Later, read from the file using the file API
+		lock = false; // Let other people read at the same time
+		FileReadChannel readChannel = fileService.openReadChannel(file, false);
+
+		// Again, different standard Java ways of reading from the channel.
+		BufferedReader reader = new BufferedReader(Channels.newReader(
+				readChannel, "UTF8"));
+		String line = reader.readLine();
+		// line = "The woods are lovely dark and deep."
+
+		readChannel.close();
+		*/
+		
 	}
 }
