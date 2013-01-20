275494187c31182f3be9668af0ab319805338929
Peter Zhang <p@peetahzee.com>
2013-01-19 20:48:36 -0500

    patching twilio

--- eclipse_project/src/com/ptzlabs/wc/servlet/TwilioServlet.java
+++ eclipse_project/src/com/ptzlabs/wc/servlet/TwilioServlet.java
@@ -1,6 +1,17 @@
 package com.ptzlabs.wc.servlet;
+import static com.googlecode.objectify.ObjectifyService.ofy;
+
 import java.io.IOException;
-import javax.servlet.http.*;
+import java.util.Date;
+import java.util.List;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.ptzlabs.wc.Reading;
+import com.ptzlabs.wc.SmsSender;
+import com.twilio.sdk.TwilioRestException;
 
 public class TwilioServlet extends HttpServlet {
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
@@ -8,7 +19,11 @@ public class TwilioServlet extends HttpServlet {
 		List<Reading> readingList = ofy().load().type(Reading.class).filter("dueDate <=", currentDate).list();
 		for (Reading reading : readingList) {
 			if (reading.lastSent.getTime() + reading.frequency >= currentDate.getTime()) {
-				sendChunk(reading);
+				try {
+					SmsSender.sendChunk(reading);
+				} catch (TwilioRestException e) {
+					e.printStackTrace();
+				}
 				reading.lastSent = currentDate;
 			}
 		}
